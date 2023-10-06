8e204170a1aeaf6d2ceae8faa5d6c44d9113d3f2
neilgiri <neilgiri88@gmail.com>
2023-10-06 14:52:57 -0400

    reverted config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withOffline = require('next-offline');
 const nextConfig = {
 	poweredByHeader: false,
 	swcMinify: true,
-  output: "standalone",
+  output: "export",
   basePath: "",
   images: {
     unoptimized: true,
